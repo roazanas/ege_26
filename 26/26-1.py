b4dd620058a26ea9cc8ee8fca2b4ffde4287285d
loafer <gysik1234@gmail.com>
2023-10-14 21:13:44 +0500

    first decision

--- 26/26-1.py
+++ 26/26-1.py
@@ -1,29 +1,26 @@
-f = open('26-111.txt')
-k = int(f.readline())
-n = int(f.readline())
-a = sorted( list(map(int, f.readline().split())) for _ in range(n) )
-f.close()
+with open('26-111.txt') as F:
+  K = int(F.readline())
+  N = int(F.readline())
+  data = []
+  for _ in range(N):
+    t0, tEnd = map(int, F.readline().split())
+    data.append( (t0, tEnd) )
 
+data.sort()
+
+tFree = [0]*K
+
+lastStart = 0
 count = 0
-lastStart, xLast = 0, -1
-for x in range(k):
-  t = [a[0]]
-  if t[-1][0] > lastStart:
-    lastStart, xLast = t[-1][0], x
-  a.pop(0)
-  for j in range(10):
-    mn = [100500, 0]
-    dl = -1
-    for i in range(len(a)):
-      if a[i][0] > t[-1][1]:
-        mn = a[i]
-        dl = i
-        break
-    if dl == -1: break
-    t.append( mn )
-    if t[-1][0] > lastStart:
-      lastStart, xLast = t[-1][0], x
-    a.pop( dl )
-  count += len(t)
+for t0, tEnd in data:
+  for k in range(K):
+    if t0 > tFree[k]:
+      count += 1
+      tFree[k] = tEnd
+      if t0 > lastStart:
+        lastStart = t0
+        kLast = k
+      break
 
-print( count, xLast + 1 )
+print( count, kLast + 1 )
+
