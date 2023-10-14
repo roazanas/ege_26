10a4c99e34a70906718465e7f5f532229661cbb2
roazanas <anonumlol@gmail.com>
2023-10-14 20:25:11 +0500

    Task numbers read is added

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 def main():
-    pass
+    with open('data.txt') as file:
+        file_lines = file.read().split('\n')
+    cells_count, pass_count = int(file_lines[0]), int(file_lines[1])    # Считаем K и N
+    passengers = [[int(i) for i in j.split()] for j in file_lines[2:]]  # и время [[<сдачи>, <освобождения>], ...]
+    print(cells_count, pass_count, passengers)
 
 
 if __name__ == '__main__':
